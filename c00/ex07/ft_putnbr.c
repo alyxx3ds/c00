bf3c32a946bb742efbef1239717d54a0c789fd1b
alyxx3ds <114713352+alyxx3ds@users.noreply.github.com>
2025-02-24 10:55:40 +0100

    Update ft_putnbr.c

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -1,30 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_nbr.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aciprian <aciprian@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/23 11:41:49 by aciprian          #+#    #+#             */
+/*   Updated: 2025/02/24 10:52:42 by aciprian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <unistd.h>
 
-void ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
-    if (nb == -2147483648)
-    {
-        write(1, "-2", 2);
-        nb = 147483648;
-    }
+	char	digit;
 
-    if (nb < 0)
-    {
-        write(1, "-", 1);
-        nb = -nb;
-    }
-
-    if (nb >= 10)
-    {
-        ft_putnbr(nb / 10);
-    }
-
-    char digit = '0' + (nb % 10);
-    write(1, &digit, 1);
+	if (nb == -2147483648)
+	{
+		write(1, "-2", 2);
+		nb = 147483648;
+	}
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+	}
+	digit = '0' + (nb % 10);
+	write(1, &digit, 1);
 }
-int main(void){
-	
+
+int	main(void)
+{
 	ft_putnbr(32);
-	
-	return 0;
+	return (0);
 }
